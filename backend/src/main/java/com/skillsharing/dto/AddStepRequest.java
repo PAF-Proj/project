069ed23950c6b23f689d3e7a434e1874e3b78185
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-04-13 14:26:24 +0530

    git commit -m "feat: implement DTOs"

---
+++ backend/src/main/java/com/skillsharing/dto/AddStepRequest.java
@@ -0,0 +1,12 @@
+package com.skillsharing.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AddStepRequest {
+    private String title;
+    private String content;
+
+}
